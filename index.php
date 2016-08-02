7e5e8c6d866790303de0334f32e298066fe7f096
Henge Cyche <hengecyche@hotmail.com>
2016-08-02 15:09:46 +1000

    Initial Commit
    Initial Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.css">
+        <script type="text/javascript" src="jquery-3.1.0.min.js" defer></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="container header" id="reader-header">
+                <div class="row">
+                    <div class="col-md-4">
+                        Manga List
+                    </div>
+                    <div class="col-md-4">
+                        Chapter List
+                    </div>
+                    <div class="col-md-4">
+                        Page Number
+                    </div>
+                </div>
+            </div>
+            <div class="container reader-setting" id="reader-setting">
+                
+            </div>
+            <div class="container reader-body" id="reader-body">
+                
+            </div>
+        </div>
+    </body>
+</html>
